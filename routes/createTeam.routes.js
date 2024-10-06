7e52ee7d681059143a99110f3e59c7bef617f8c7
Sahil <sahilshangloo35@gmail.com>
2024-10-06 12:27:33 +0530

    Created /all-members endpoint in teamRouter to get members of a team

--- routes/createTeam.routes.js
+++ routes/createTeam.routes.js
@@ -49,4 +49,31 @@ teamRouter.post("/create", verifyToken, async (req, res) => {
     }
 })
 
+teamRouter.get("/all-members", verifyToken, async (req, res) => {
+    try {
+
+        const userId = req.body.admin._id;
+
+        const teams = await TeamModel.find({ teamMembers: userId }).populate("teamMembers", "name email rollNo branch year");
+
+        if (!teams.length) {
+            return res.status(404).json({ message: "No teams found for this user." });
+        }
+
+        const teamsWithMembers = teams.map(team => ({
+            teamName: team.teamName,
+            description: team.description,
+            members: team.teamMembers,
+        }));
+
+        return res.status(200).json({
+            message: "Teams and members retrieved successfully",
+            teams: teamsWithMembers,
+        });
+    } catch (error) {
+        console.error("Error retrieving members:", error);
+        return res.status(500).json({ message: "Internal server error" });
+    }
+})
+
 export default teamRouter
